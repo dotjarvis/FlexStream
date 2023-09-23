96f9037e15650c49c820c148453644076d18556d
dotjarvis <johnsonmakings@gmail.com>
2023-09-23 16:20:35 +0300

    Added and handled form submission with NETLIFY

--- script.js
+++ script.js
@@ -364,3 +364,26 @@ window.addEventListener("resize", () => {
 
   player.setSize(playerWidth, playerHeight);
 });
+
+// HADNINGLIG FORMS
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => alert("Sucessfully subscribed"))
+    .catch((error) => alert(error));
+
+  document.querySelector(".email-input").value = "";
+};
+
+document
+  .querySelector(".email-holder")
+  .addEventListener("submit", handleSubmit);
